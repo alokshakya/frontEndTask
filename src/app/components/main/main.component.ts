b42922a8cac8c564a5b9166135c2ccab5751efc3
Alok Kumar <alok.kumar@incedoinc.com>
2020-08-16 17:45:42 +0530

    route query params handled

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+  limit: number;
+  launchYear: number;
+  launchSuccess: boolean;
+  landSuccess: boolean;
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      console.log('params ', params);
+    });
   }
 
 }
